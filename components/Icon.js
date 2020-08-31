ea51c3588c5c813b272b22dc6911e01493be2723
Matheus Alvim <60630664+Alvimatheus@users.noreply.github.com>
2020-08-31 19:31:19 -0300

    Sidebar - Completed

---
+++ components/Icon.js
@@ -0,0 +1,82 @@
+import React from 'react'
+
+export default function Icon(props) {
+    return (
+        <div className="wrapper">
+
+            {props.min == true ? <div className="user-photo"></div> : <><div className="user-photo"></div>
+                <div className="user-info">
+                <h4>{props.userName || "User Name"}</h4>
+                <h6>{props.userGame || "Game Name"}</h6>
+            </div>
+            {props.userOnline == true ? <div className="user-views"><div className="online"></div>{props.userViews}</div> : "Offline"}</>}  
+
+            <style jsx>{`
+            .wrapper{
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                background-color: black;
+                color: white;
+                width: 100%;
+                padding-bottom: 8px;
+            }
+
+            .resolver-a-merda{
+                padding-left: 5px;
+            }
+
+            .user-photo{
+                background: url(${props.userPhoto || "https://barcarena.pa.gov.br/portal/img/perfil/padrao.jpg"}) no-repeat center center / cover;
+                width: 40px;
+                height: 40px;
+                border-radius: 100%;
+            }
+
+            .user-info{
+                flex: 1;
+                margin-left: 10px;
+                font-family: 'Balsamiq Sans', cursive;
+                font-family: 'Nunito', sans-serif;
+
+            }
+
+            h4{
+                margin: 0px;
+                font-size: 15px;
+                padding-bottom: 3px;
+                font-family: 'Balsamiq Sans', cursive;
+                font-family: 'Nunito', sans-serif;
+            }
+
+            h6{
+                margin: 0px;
+                font-size: 10px;
+                font-family: 'Balsamiq Sans', cursive;
+                font-family: 'Nunito', sans-serif;
+            }
+
+            .user-views{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding-right: 8px;
+                font-size: 15px;
+                font-family: 'Balsamiq Sans', cursive;
+                font-family: 'Nunito', sans-serif;
+
+            }
+
+            .online{
+                background-color: red;
+                width: 8px;
+                height: 8px;
+                border-radius: 100%;
+                margin: 3px;
+            }
+
+            `}</style>
+
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+export default function index() {
+    return (
+        <div>
+            
+            <Sidebar />
+
+
+
+
+
+
+        <style global jsx>{`
+        body{
+            margin: 0px;
+        }
+        `}</style>
+
+        </div>
+    )
+}
